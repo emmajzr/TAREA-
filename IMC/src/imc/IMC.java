47074ecce4b0ae9d1d5dcebc2232deb1ead39026
imanol PS <imanolperruno@gmail.com>
2024-07-24 19:21:35 -0600

    Tarea de Imanol

--- IMC/src/imc/IMC.java
+++ IMC/src/imc/IMC.java
@@ -8,23 +8,6 @@ public class IMC {
 
     public static void main(String[] args) {
 
-      float peso,altura;
-
-      Scanner teclado =new Scanner (System.in);
-
-      System.out.println("Ingrese su peso en kilogramos");
-
-      peso = teclado.nextFloat();
-
-      System.out.println("Ingrese su altura en metros");
-
-      altura = teclado.nextFloat();
-
-      double IMC =peso/(altura*altura);
-
-      System.out.println("Su IMC es"+ IMC);
-
-      teclado.close();
 
     }
 
